615960e95b1c22ea2307f516d3c3012c6c532fd6
MoistButteryBiscuits <Mikeleblanc8489@gmail.com>
2022-01-24 19:50:27 -0500

    Add basic operators

---
+++ script.js
@@ -0,0 +1,15 @@
+function addNumbers (variableOne, variableTwo) {
+    return variableOne + variableTwo;
+}
+
+function subtractNumbers (variableOne, variableTwo) {
+    return variableOne - variableTwo;
+}
+
+function multiplyNumbers (variableOne, variableTwo) {
+    return variableOne * variableTwo;
+}
+
+function divideNumbers (variableOne, variableTwo) {
+    return variableOne / variableTwo;
+}
